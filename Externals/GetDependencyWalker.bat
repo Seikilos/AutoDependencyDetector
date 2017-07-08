a02fe24462aaa9988ebec75e1d5b085da33106d8
Seikilos <spam@endstadium.com>
2017-07-08 20:29:19 +0200

    Added x86 and x64 versions.

--- Externals/GetDependencyWalker.bat
+++ Externals/GetDependencyWalker.bat
@@ -1,4 +1,9 @@
 powershell -Command "Invoke-WebRequest http://www.dependencywalker.com/depends22_x64.zip -OutFile depends.zip"
-powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('depends.zip', 'DependencyWalker'); }"
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('depends.zip', 'DependencyWalker\x64'); }"
 
+del depends.zip
+
+
+powershell -Command "Invoke-WebRequest http://www.dependencywalker.com/depends22_x86.zip -OutFile depends.zip"
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('depends.zip', 'DependencyWalker\x86'); }"
 del depends.zip
