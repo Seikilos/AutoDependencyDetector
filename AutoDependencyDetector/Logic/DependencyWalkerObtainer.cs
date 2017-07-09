9c5f7f2be5dbe4067678105ab70e9cb7e79e2c11
Seikilos <spam@endstadium.com>
2017-07-09 08:52:23 +0200

    Added dependency walker obtainer. Downloads dependency walker to specified location.

---
+++ AutoDependencyDetector/Logic/DependencyWalkerObtainer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDependencyDetector.Logic
+{
+    public class DependencyWalkerObtainer
+    {
+        public string DirectoryToDownload { get; }
+        public DependencyWalkerObtainer(string directoryToPlace)
+        {
+            if ( Directory.Exists( directoryToPlace ) == false )
+            {
+                Directory.CreateDirectory( directoryToPlace );
+            }
+
+            DirectoryToDownload = directoryToPlace;
+        }
+
+        public async Task DownloadFiles()
+        {
+            await _downloadFile( "http://www.dependencywalker.com/depends22_x64.zip", Path.Combine( DirectoryToDownload, "x64" ) );
+            await _downloadFile( "http://www.dependencywalker.com/depends22_x86.zip", Path.Combine( DirectoryToDownload, "x86" ) );
+        }
+
+        private async Task _downloadFile( string url, string destination )
+        {
+            var tempFile = Path.GetTempFileName();
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.Credentials = CredentialCache.DefaultNetworkCredentials;
+                await webClient.DownloadFileTaskAsync(new Uri(url), tempFile);
+            }
+            _extract( tempFile, destination );
+
+            File.Delete( tempFile );
+        }
+
+        private void _extract( string tempFile, string destination )
+        {
+            ZipFile.ExtractToDirectory( tempFile, destination );
+        }
+    }
+}
---
+++ AutoDependencyDetectorTests/TestDependencyWalkerObtainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoDependencyDetector.Logic;
+using NUnit.Framework;
+
+namespace AutoDependencyDetectorTests
+{
+    public class TestDependencyWalkerObtainer : TestCommon
+    {
+        [Test]
+        public async Task Test_DependencyWalkerObtainer_fetches_x64_and_x86()
+        {
+            var path = Path.Combine( TestData, "new_dir" );
+
+            var d = new DependencyWalkerObtainer(path);
+
+            await d.DownloadFiles();
+
+            Assert.That( Directory.GetFiles( path, "*.exe", SearchOption.AllDirectories ), Has.Exactly( 2 ).Items );
+        }
+
+    }
+}
