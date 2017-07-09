374ae2661e5cb81453579732651c626681b5c554
Seikilos <spam@endstadium.com>
2017-07-09 09:12:06 +0200

    Added dependency walker downloader executable run on build of /t:DownloadDependencyWalker

---
+++ DependencyWalkerDownloader/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoDependencyDetector.Logic;
+
+namespace DependencyWalkerDownloader
+{
+    /// <summary>
+    /// Helper to actually download dependency walker to external. Must be run separately in order to get dependency walker. This is machine independent
+    /// and should also work on linux based build nodes
+    /// </summary>
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            try
+            {
+                Execute( args );
+            }
+            catch ( Exception e )
+            {
+                Console.WriteLine( e );
+                Environment.Exit( 1 );
+            }
+        }
+
+        static void Execute(string[] args)
+        {
+            if ( args.Length != 1 )
+            {
+                throw new ArgumentException("First argument must be directory for dependency walker");
+            }
+
+            var path = args[ 0 ];
+
+            
+
+            Console.WriteLine( $"Downloading depends to {path}" );
+
+            var d = new DependencyWalkerObtainer(path);
+
+            d.DownloadFiles().GetAwaiter().GetResult();
+        }
+    }
+}
